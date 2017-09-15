344908672716989d3596e96d04942622202da4d1
hepan <he_pann@163.com>
2017-09-15 09:42:30 +0800

    布局

--- app/src/main/java/com/nf/hpsweats/MainActivity.java
+++ app/src/main/java/com/nf/hpsweats/MainActivity.java
@@ -1,13 +1,24 @@
 package com.nf.hpsweats;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.btn_scan)
+    Button btnScan;
+    @Bind(R.id.lv_devices)
+    ListView lvDevices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
